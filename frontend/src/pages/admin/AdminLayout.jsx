2a912b517d69d19f3a8d459c9dbaf3497e689da1
minhmh <minh.maihoang.71@gmail.com>
2026-05-01 19:11:01 +0700

    fix: admin_layout Nam Gau

--- frontend/src/pages/admin/AdminLayout.jsx
+++ frontend/src/pages/admin/AdminLayout.jsx
@@ -71,6 +71,8 @@ const AdminLayout = () => {
         navigate("/admin/hrm-dashboard", { replace: true });
       } else if (userRole === 1) {
         navigate("/admin/dashboard", { replace: true });
+      }
+    }
     let isMounted = true;
 
     const fetchEmployeeProfile = async () => {
@@ -90,13 +92,13 @@ const AdminLayout = () => {
         }
       }
     };
-
+ 
     fetchEmployeeProfile();
 
     return () => {
       isMounted = false;
     };
-  }, [userRole]);
+  }, [userRole, navigate]);
 
   useEffect(() => {
     if ([4, 5].includes(Number(userRole)) && employeeProfile === undefined) {
